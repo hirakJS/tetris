d7ab4238708425a32606dedc822681d54769acb0
Hirakjyoti Sarma <5646035+hirakJS@users.noreply.github.com>
2020-10-04 15:23:19 +0530

    Add collision detection

--- piece.js
+++ piece.js
@@ -3,8 +3,8 @@ class Piece {
         this.ctx = ctx;
         this.color = 'red';
         this.shape = [
-            [1, 0, 0],
-            [1, 1, 1],
+            [1, 1, 0],
+            [1, 1, 0],
             [0, 0, 0]
         ];
 
